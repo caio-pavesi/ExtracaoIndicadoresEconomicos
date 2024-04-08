d1f9a5b001bce123a2aa5e17ea6a2928c7a4c3d8
Caio Pavesi <91769150+caio-pavesi@users.noreply.github.com>
2024-04-08 09:54:41 -0300

    Removed OneDrive and User paths, Added DIR and Data paths

--- App/Config/folders.py
+++ App/Config/folders.py
@@ -1,5 +1,5 @@
 from os import path
 from pathlib import Path
 
-USER = Path(path.expanduser('~'))
-ONEDRIVE = Path(f"C:/Users/{ USER.parts[-1] }/XP Investimentos")
+DIR = Path(__file__).parent
+DATA = DIR / "Data"
